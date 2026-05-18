8567e8e2ac27c78f58bd06a2826ab6c581222913
Harpreet Kaur <harpreetkaur13122k@gmail.com>
2026-05-18 15:14:25 +0530

    Fixed SSR config

--- src/app/environment/environmentLocal.ts
+++ src/app/environment/environmentLocal.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   envName: 'local',
- // appUrl : 'https://localhost:7074/'
-  appUrl : 'https://localhost:44374/'
+  appUrl : 'https://localhost:7074/'
+ // appUrl : 'https://localhost:44374/'
 
  
 
